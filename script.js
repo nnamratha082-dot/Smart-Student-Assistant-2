0aa5c67ed479e05b3b1b99af9bf3392ff4df5b29
nnamratha082-dot <nnamratha082@gmail.com>
2026-07-20 20:30:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+function login() {
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+
+    if (user === "namratha" && pass === "1234") {
+        window.location.href = "home.html";
+    } else {
+        alert("Invalid Username or Password");
+    }
+}
+
+function calculator() {
+    alert("Calculator Coming Soon 😊");
+}
+
+function notes() {
+    alert("Notes Coming Soon 📝");
+}
+
+function about() {
+    alert("My First Electron App\nCreated by Namratha ❤️");
+}
+
+function logout() {
+    window.location.href = "index.html";
+}
+function add() {
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+    document.getElementById("result").innerHTML = "Answer: " + (a + b);
+}
+
+function sub() {
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+    document.getElementById("result").innerHTML = "Answer: " + (a - b);
+}
+
+function mul() {
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+    document.getElementById("result").innerHTML = "Answer: " + (a * b);
+}
+
+function div() {
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+
+    if (b === 0) {
+        document.getElementById("result").innerHTML = "Cannot divide by zero";
+    } else {
+        document.getElementById("result").innerHTML = "Answer: " + (a / b);
+    }
+}
